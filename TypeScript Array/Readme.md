6d470db51863617a2c49d8fed67a1ee1f4fd9427
ISRA ILYAS <israqureshi138@gmail.com>
2024-10-19 18:58:33 +0500

    array

---
+++ TypeScript Array/Readme.md
@@ -0,0 +1,13 @@
+# Array
+
+In TypeScript, an array is a date tpe that can store multiple values of different data types sequentially.
+
+## Methods to define array
+
+1- using square brackets
+2- using the array constructor
+3- using the array.of method
+
+## Operations: Accessing elements using index:
+
+You can access individual elements of an array using square brackets notation with the index of the element. Remember that array indices start from zero.
---
+++ TypeScript Array/array.js
@@ -0,0 +1,14 @@
+// 1- using square brackets
+var numbers = [1, 2, 4, 5, 6];
+//2- using the array constructor
+// let numbers1:number[] = new Array(1,2,3,4,5,6)
+// 3- using the array.of method
+// let numbers2:number[] = Array.of(1,2,3,4,5,6,7)
+// let names = Array.of("isra",2,"hello","el") // Error
+// let names:string[] = Array.of("isra","ilyas","qureshi")
+// Operations: Accessing elements using index:
+console.log(numbers[0]);
+console.log(numbers[1]);
+// console.log(numbers[2])
+// Array methods
+console.log("Lenth of array numbers is ".concat(numbers.length, "."));
